086963aa2cb7075e2c0a6f8bd2b5e6f9db578798
lisannbecker <116002694+lisannbecker@users.noreply.github.com>
2023-11-29 11:37:37 +0100

    Upload part 2

---
+++ A2T2/vp.py
@@ -0,0 +1,131 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2022 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2022
+# Date Created: 2022-11-14
+################################################################################
+
+"""Defines various kinds of visual-prompting modules for images."""
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class FixedPatchPrompter(nn.Module):
+    """
+    Defines visual-prompt as a fixed patch over an image.
+    For refernece, this prompt should look like Fig 2(a) in the PDF.
+    """
+    def __init__(self, args):
+        super(FixedPatchPrompter, self).__init__()
+
+        assert isinstance(args.image_size, int), "image_size must be an integer"
+        assert isinstance(args.prompt_size, int), "prompt_size must be an integer"
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        # TODO: Define the prompt parameters here. The prompt is basically a
+        # patch (can define as self.patch) of size [prompt_size, prompt_size]
+        # that is placed at the top-left corner of the image.
+
+        # Hints:
+        # - The size of patch needs to be [1, 3, prompt_size, prompt_size]
+        #     (1 for the batch dimension)
+        #     (3 for the RGB channels)
+        # - You can define variable parameters using torch.nn.Parameter
+        # - You can initialize the patch randomly in N(0, 1) using torch.randn
+
+        self.patch = nn.Parameter(torch.randn(1, 3, args.prompt_size, args.prompt_size)) #tensor(Batch size, channels, height, width) wrapped in parameter
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        # TODO: For a given batch of images, place the patch at the top-left
+
+        # Hints:
+        # - First define the prompt. 
+        batch_size = len(x)
+        prompt = self.patch.repeat(batch_size, 1, 1, 1) #replicate patch along batch dimension - everything else unchanged
+
+        # Then add it to the batch of images
+        x[:, :, :self.patch.size(2), :self.patch.size(3)] += prompt #select top-left portion of each image (size of the patch) and add the patch
+
+        # - It is always advisable to implement and then visualize if
+        #   your prompter does what you expect it to do.
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+class PadPrompter(nn.Module):
+    """
+    Defines visual-prompt as a parametric padding over an image.
+    For refernece, this prompt should look like Fig 2(c) in the PDF.
+    """
+    def __init__(self, args):
+        super(PadPrompter, self).__init__()
+        pad_size = args.prompt_size
+        image_size = args.image_size
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        # TODO: Define the padding as variables self.pad_left, self.pad_right, self.pad_up, self.pad_down
+
+        # Hints:
+        # - Each of these are parameters that we need to learn. So how would you define them in torch?
+        # - See Fig 2(c) in the assignment to get a sense of how each of these should look like.
+        # - Shape of self.pad_up and self.pad_down should be (1, 3, pad_size, image_size)
+        self.pad_up = nn.Parameter(torch.randn(1, 3, pad_size, image_size)) #batch, channels, pad, image
+        self.pad_down = nn.Parameter(torch.randn(1, 3, pad_size, image_size)) #batch, channels, pad, image
+
+        # - See Fig 2.(g)/(h) and think about the shape of self.pad_left and self.pad_right
+        self.pad_left = nn.Parameter(torch.randn(1, 3, (image_size - pad_size*2), pad_size)) #batch, channels, height: image - 2*padding, width:pad
+        self.pad_right = nn.Parameter(torch.randn(1, 3, (image_size - pad_size*2), pad_size)) #batch, channels, pad, image - 2*padding; as these are covered by pad_up and pad_down already
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        # TODO: For a given batch of images, add the prompt as a padding to the image.
+
+        # Hints:
+        # - First define the prompt. Then add it to the batch of images.
+        batch_size = len(x)
+        prompt_up = self.pad_up.repeat(batch_size, 1, 1, 1) 
+        prompt_down = self.pad_down.repeat(batch_size, 1, 1, 1) 
+        prompt_left = self.pad_left.repeat(batch_size, 1, 1, 1) 
+        prompt_right = self.pad_right.repeat(batch_size, 1, 1, 1)
+
+        x[:, :, :prompt_up.size(2), :] += prompt_up #batch, channels, height, width
+        x[:, :, -prompt_down.size(2):, :] += prompt_down #third dim = height starts from the bottom and moves upwards
+
+        x[:, :, prompt_up.size(2):-prompt_down.size(2), :prompt_left.size(3)] += prompt_left #prompt should start at height: 0 + height of pad_up; width:0 and have the dimensions specified above
+        x[:, :, prompt_up.size(2):-prompt_down.size(2), -prompt_right.size(3):] += prompt_right #prompt should start at height: 0 + height of pad_up; width: image width - pad_size of pad_right
+
+
+        # - It is always advisable to implement and then visualize if
+        #   your prompter does what you expect it to do.
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
---
+++ A2T2/vpt_model.py
@@ -0,0 +1,162 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2022 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2022
+# Date Created: 2022-11-14
+################################################################################
+
+"""Defines the VisualPrompting model (based on CLIP)"""
+from pprint import pprint
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+
+from clip import clip
+from vp import (
+    PadPrompter,
+    FixedPatchPrompter,
+)
+#my additions
+import torch.nn.functional as F
+
+PROMPT_TYPES = {
+    "padding": PadPrompter,
+    "fixed_patch": FixedPatchPrompter,
+}
+
+
+def load_clip_to_cpu(cfg):
+    """Loads CLIP model to CPU."""
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+
+    try:
+        # loading JIT archive
+        model = torch.jit.load(model_path, map_location="cpu").eval()
+        state_dict = None
+
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location="cpu")
+
+    model = clip.build_model(state_dict or model.state_dict())
+
+    return model
+
+
+class VisualPromptCLIP(nn.Module):
+    """Modified CLIP module to support prompting."""
+    def __init__(self, args, dataset, template="This is a photo of {}"):
+        super(VisualPromptCLIP, self).__init__()
+        classnames = dataset.classes
+
+        print(f"Loading CLIP (backbone: {args.arch})")
+        clip_model = self.load_clip_to_cpu(args)
+        clip_model.to(args.device)
+
+        # Hack to make model as float() (This is a CLIP hack)
+        if args.device == "cpu":
+            clip_model = clip_model.float()
+
+        prompts = [template.format(c.replace("_", " ")) for c in classnames]
+        print("List of prompts:")
+        print(prompts)
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        # TODO: Write code to compute text features.
+        # Hint: You can use the code from clipzs.py here!
+
+        # Instructions:
+        # - Given a list of prompts, compute the text features for each prompt.
+        tokenized_text = clip.tokenize(prompts)
+
+        text_features = clip_model.encode_text(tokenized_text)
+        text_features = (text_features - text_features.mean()) / text_features.std() 
+
+        # - Return a tensor of shape (num_prompts, 512).
+        #print(f"Shape text features: {text_features.size()} - should be (num_prompts, 512)")
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        self.text_features = text_features
+        self.clip_model = clip_model
+        self.logit_scale = self.clip_model.logit_scale.exp().detach()
+
+        assert args.method in PROMPT_TYPES, f"{args.method} is not supported :)!"
+        self.prompt_learner = PROMPT_TYPES[args.method](args)
+
+        if args.visualize_prompt:
+            self.visualize_prompt(args.method)
+
+    def forward(self, image):
+        """Forward pass of the model."""
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        # TODO: Implement the forward function. This is not exactly the same as
+        # the model_inferece function in clipzs.py! Please see the steps below.
+
+        # Steps:
+        # - [!] Add the prompt to the image using self.prompt_learner.
+        prompted_img = self.prompt_learner(image)#[0].cpu()
+
+        # - Compute the image features using the CLIP model.
+        img_features = self.clip_model.encode_image(image)
+
+        # - Normalize the image features.
+        normalized_img_features = (img_features - img_features.mean()) / img_features.std()
+
+        # - Compute similarity logits between the image features and the text features.
+        similarity_logits = F.cosine_similarity(normalized_img_features.unsqueeze(1), self.text_features.unsqueeze(0), dim=-1)
+
+        # - You need to multiply the similarity logits with the logit scale (clip_model.logit_scale).
+        similarity_logits *= self.clip_model.logit_scale
+        
+        # - Return logits of shape (batch size, number of classes).
+        return similarity_logits
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def load_clip_to_cpu(self, args):
+        """Loads CLIP model to CPU."""
+        backbone_name = args.arch
+        url = clip._MODELS[backbone_name]
+        model_path = clip._download(url, args.root)
+        try:
+            # loading JIT archive
+            model = torch.jit.load(model_path, map_location="cpu").eval()
+            state_dict = None
+        except RuntimeError:
+            state_dict = torch.load(model_path, map_location="cpu")
+
+        model = clip.build_model(state_dict or model.state_dict())
+        return model
+
+    @torch.no_grad()
+    def visualize_prompt(self, method):
+        """Visualizes the prompt."""
+        fake_img = torch.ones(1, 3, 224, 224)
+        prompted_img = self.prompt_learner(fake_img)[0].cpu()
+        prompted_img = torch.clamp(prompted_img, 0, 1)
+
+        print("Visualizing prompt...")
+        plt.imsave(f"prompt_{method}.png", prompted_img.permute(1, 2, 0).numpy())
